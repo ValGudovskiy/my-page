b61821225d8ccf0cc685c9f83e7b50021edd66dd
Гудовский Валерий <v.v.gudovskiy@gmail.com>
2023-10-17 20:19:45 +0300

    /

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// const root = document.querySelector("#root");
+
+// function createTeg(arrChildren, parent) {
+//   const node = arrChildren.map((el) => {
+//     const tag = document.createElement(el[0]);
+//     tag.classList = el[1];
+//     parent.appendChild(tag);
+//     return tag
+//   });
+//   return node;
+// }
+
+// const arrEl = createTeg(
+//   [
+//     ["header", "header"],
+//     ["main", "main"],
+//     ["footer", "footer"],
+//   ],
+//   root
+// );
+// console.log(arrEl)
+// const elHead = createTeg(
+//   [
+//     ["div", "photo"],
+//     ["p", "text"],
+//   ],
+//   arrEl[0]
+// );
